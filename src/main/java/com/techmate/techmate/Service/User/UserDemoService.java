6ca521ad126e2abae75652dd4f43d12d66c88d2d
jafeth gamboa <jafethgamboabaas@gmail.com>
2025-11-08 14:22:39 -0600

    chore: remove uppercase duplicate paths, keep lowercase folders

--- src/main/java/com/techmate/techmate/Service/User/UserDemoService.java
+++
@@ -1,9 +0,0 @@
-package com.techmate.techmate.service.User;
-
-import java.util.Optional;
-
-import com.techmate.techmate.dto.UsuarioDTO;
-public interface UserDemoService {
-Optional<UsuarioDTO> getUserDetailsFromToken(String token);
-}
-
